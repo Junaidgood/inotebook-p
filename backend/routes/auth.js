eedbd85861e1e41f55e8279400cef95c108d5bdc
M Junaid Siddiqi <mjunaidsiddiqi334@gmail.com>
2025-01-30 13:51:56 +0500

    mongodb
    update save in mongodb

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
-router.get('/', (req,res)=>{
-    obj = {
-        a: 'this',
-        number: 70
-    }
-    res.json(obj)
+router.post('/', (req,res)=>{
+    const user= new User(req.body)
+    console.log(req.body);
+    user.save()
+    res.send(req.body)
+    
 })
 
 module.exports = router
