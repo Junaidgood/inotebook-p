a7be5c509c542517bec437d65d4fd5a75bffc80f
M Junaid Siddiqi <mjunaidsiddiqi334@gmail.com>
2025-02-05 22:33:50 +0500

    jwtToken
    JwtToken implement

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = 'goodwork@junaid';
+
 
 router.post('/createuser', [
     body('name','Enter a valid name').isLength({ min: 3 }),
@@ -20,13 +24,23 @@ router.post('/createuser', [
     if(user){
         return res.status(400).json({error:"sorry a user with this email already exists"})
     } 
-    user = await User.create({
+    
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password,salt);
         //create a new user
+        user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email:req.body.email
     })
     // catch errors
+    const data = {
+        user:{
+            id: user.id
+        }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET)
+    res.json({authtoken})
     res.json(user)
     } catch(error){
         console.error(error.message);
