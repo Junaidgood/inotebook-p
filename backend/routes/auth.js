7bc573027e0e0f94292a33d279cb55ef42ab0825
M Junaid Siddiqi <mjunaidsiddiqi334@gmail.com>
2025-02-06 03:18:20 +0500

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const JWT_SECRET = 'goodwork@junaid';
 
-
+//authenticate a user using post :/api/auth/createuser , no login required
 router.post('/createuser', [
     body('name','Enter a valid name').isLength({ min: 3 }),
     body('email','Enter a valid email').isEmail(),
@@ -45,6 +45,18 @@ router.post('/createuser', [
     } catch(error){
         console.error(error.message);
         res.status(500).send("some error occured")
+    }
+})
+
+router.post('/createuser', [
+    
+    body('email','Enter a valid email').isEmail(),
+    body('password', 'password cannot be blank').exists()
+    
+], async(req, res) =>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
     }
 })
 
