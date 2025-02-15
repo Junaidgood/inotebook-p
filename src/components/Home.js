5adccec54fdb7de4487efdb78220989a277a7bbd
M Junaid Siddiqi <mjunaidsiddiqi334@gmail.com>
2025-02-15 22:30:03 +0500

    context Note.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,7 @@
-import React, {useContext}from 'react'
-import noteContext from '../context/Note/NoteContext';
+import Notes from "./Notes"
 
 export const Home = () => {
-  const context = useContext(noteContext)
-  const {notes, setNotes} = context;
+  
   return (
     <div>
       <div className="container my-3">
@@ -25,14 +23,8 @@ export const Home = () => {
           <button type="submit" class="btn btn-primary">Submit</button>
         </form>
       </div>
-      <div className="container">
-      <h1>Your Notes</h1>
-      {
-        notes.map((note)=>{
-        return note.title
-      })}
+      <Notes/>
       
-      </div>
     </div>
   )
 }
--- src/context/Note/NoteState.js
+++ src/context/Note/NoteState.js
@@ -21,6 +21,42 @@ const NoteState = (props)=>{
             "tag": "good",
             "date": "2025-02-10T21:07:17.198Z",
             "__v": 0
+        },
+        {
+            "_id": "67aa6a85b9e84b1ec4f90ec3",
+            "user": "67a3a029e199b43775f9ba67",
+            "title": "My title",
+            "description": "Jeo to asy",
+            "tag": "good",
+            "date": "2025-02-10T21:07:17.198Z",
+            "__v": 0
+        },
+        {
+            "_id": "67aa6a85b9e84b1ec4f90ec3",
+            "user": "67a3a029e199b43775f9ba67",
+            "title": "My title",
+            "description": "Jeo to asy",
+            "tag": "good",
+            "date": "2025-02-10T21:07:17.198Z",
+            "__v": 0
+        },
+        {
+            "_id": "67aa6a85b9e84b1ec4f90ec3",
+            "user": "67a3a029e199b43775f9ba67",
+            "title": "My title",
+            "description": "Jeo to asy",
+            "tag": "good",
+            "date": "2025-02-10T21:07:17.198Z",
+            "__v": 0
+        },
+        {
+            "_id": "67aa6a85b9e84b1ec4f90ec3",
+            "user": "67a3a029e199b43775f9ba67",
+            "title": "My title",
+            "description": "Jeo to asy",
+            "tag": "good",
+            "date": "2025-02-10T21:07:17.198Z",
+            "__v": 0
         }
     ]
 
