0ec5d8547b266648c788b6a7123db52767c72034
M Junaid Siddiqi <mjunaidsiddiqi334@gmail.com>
2025-02-20 03:36:43 +0500

    working on delete button

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,14 +1,15 @@
 import React, {useContext} from 'react'
-import noteContext from '../context/Note/NoteContext';
+
 import Noteitem from './Noteitem';
 import Addnote from './Addnote';
+import noteContext from '../context/Note/NoteContext';
 
 const Notes = () => {
     const context = useContext(noteContext)
-  const {notes,addNote} = context;
+  const {notes} = context;
   return (
     <>
-    <Addnote/>
+    
     <div className="row my-3">
       <h1>Your Notes</h1>
       {
--- src/context/Note/NoteState.js
+++ src/context/Note/NoteState.js
@@ -78,11 +78,13 @@ const NoteState = (props)=>{
      setNotes(notes.concat(note))
     }
     //Delete a note
-    const deleteNote = () =>{
-        
+    const deleteNote = (id) =>{
+        console.log("deleting the note with id" + id) 
+        const newNotes = notes.filter((note)=>{return note._id!==id})
+        setNotes(newNotes)
     }
     //Edit a note
-    const editNote = () =>{
+    const editNote = (id,title,description,tag) =>{
         
     }
     return(
